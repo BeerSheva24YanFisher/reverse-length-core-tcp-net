ffc93af435ff5a07f659f360f3084b2369b11a80
yanfi-sher <loss.soul20@gmail.com>
2024-11-05 22:02:44 +0200

    hw 38

--- reverse-length-client-tcp-net/src/main/java/telran/appl/net/MainClient.java
+++ reverse-length-client-tcp-net/src/main/java/telran/appl/net/MainClient.java
@@ -1,7 +1,33 @@
 package telran.appl.net;
 
+import java.util.Scanner;
+
+import telran.net.TcpClient;
+
 public class MainClient {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String host = "localhost";
+        int port = 5000;
+
+        try (TcpClient client = new TcpClient(host, port)) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter text to reverse and get its length (type 'exit' to quit):");
+
+            while (true) {
+                System.out.print("Input: ");
+                String input = scanner.nextLine();
+                
+                if ("exit".equalsIgnoreCase(input)) {
+                    System.out.println("Exiting...");
+                    break;
+                }
+
+                String response = client.sendAndReceive("reverseLength", input);
+                System.out.println("Response from server: " + response);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Client error: " + e.getMessage());
+        }
     }
 }
--- reverse-length-server-tcp-net/src/main/java/telran/appl/net/MainServer.java
+++ reverse-length-server-tcp-net/src/main/java/telran/appl/net/MainServer.java
@@ -1,7 +1,17 @@
 package telran.appl.net;
 
+import telran.net.Protocol;
+import telran.net.TcpServer;
+
 public class MainServer {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int port = 5000;
+
+        Protocol protocol = new ReverseLengthProtocol();
+        
+        TcpServer server = new TcpServer(protocol, port);
+        new Thread(server).start();
+
+        System.out.println("Reverse Length Server started on port " + port);
     }
 }
