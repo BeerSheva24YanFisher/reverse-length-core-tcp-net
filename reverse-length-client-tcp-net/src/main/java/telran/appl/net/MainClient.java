e40c1d425f4f892a6ce60cc3349040a11979d846
yanfi-sher <loss.soul20@gmail.com>
2024-11-06 13:35:09 +0200

    hw 38

--- reverse-length-client-tcp-net/src/main/java/telran/appl/net/MainClient.java
+++ reverse-length-client-tcp-net/src/main/java/telran/appl/net/MainClient.java
@@ -1,33 +1,52 @@
 package telran.appl.net;
 
-import java.util.Scanner;
+import java.io.IOException;
 
 import telran.net.TcpClient;
+import telran.view.InputOutput;
+import telran.view.Item;
+import telran.view.Menu;
+import telran.view.StandardInputOutput;
 
 public class MainClient {
+    static TcpClient client;
+
     public static void main(String[] args) {
-        String host = "localhost";
-        int port = 5000;
+        Menu menu = new Menu("Network application",
+                Item.of("Start session", MainClient::startSession),
+                Item.of("Exit", MainClient::exit, true));
+        menu.perform(new StandardInputOutput());
+    }
 
-        try (TcpClient client = new TcpClient(host, port)) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter text to reverse and get its length (type 'exit' to quit):");
+    static void startSession(InputOutput io) {
+        String host = io.readString("Enter hostname");
+        int port = io.readNumberRange("Enter port (3000-50000)", "Wrong port", 3000, 50000).intValue();
+        tryingToClose();
+        client = new TcpClient(host, port);
+        Menu menu = new Menu("Run Session",
+                Item.of("Enter command and data", MainClient::stringProcessing),
+                Item.ofExit());
+        menu.perform(io);
+    }
 
-            while (true) {
-                System.out.print("Input: ");
-                String input = scanner.nextLine();
-                
-                if ("exit".equalsIgnoreCase(input)) {
-                    System.out.println("Exiting...");
-                    break;
-                }
+    static void stringProcessing(InputOutput io) {
+        String requestType = io.readString("Enter your command (reverse, lenght)");
+        String requestData = io.readString("Enter your data");
+        String response = client.sendAndReceive(requestType, requestData);
+        io.writeLine(response);
+    }
 
-                String response = client.sendAndReceive("reverseLength", input);
-                System.out.println("Response from server: " + response);
+    static void exit(InputOutput io) {
+        tryingToClose();
+    }
+
+    private static void tryingToClose() {
+        if (client != null) {
+            try {
+                client.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-
-        } catch (Exception e) {
-            System.out.println("Client error: " + e.getMessage());
         }
     }
 }
--- reverse-length-server-tcp-net/src/main/java/telran/appl/net/MainServer.java
+++ reverse-length-server-tcp-net/src/main/java/telran/appl/net/MainServer.java
@@ -1,17 +1,33 @@
 package telran.appl.net;
 
 import telran.net.Protocol;
+import telran.net.Request;
+import telran.net.Response;
+import telran.net.ResponseCode;
 import telran.net.TcpServer;
 
 public class MainServer {
+    private static final int SERVER_PORT = 4000;
+
+    static Protocol protocol = new Protocol() {
+        @Override
+        public Response getResponse(Request request) {
+            String requestData = request.requestData();
+            ResponseCode responseCode = ResponseCode.OK;
+            String responseData = switch (request.requestType()) {
+                case "reverse" -> new StringBuilder(requestData).reverse().toString();
+                case "length" -> String.valueOf(requestData.length());
+                default -> {
+                    responseCode = ResponseCode.WRONG_TYPE;
+                    yield "Wrong command";
+                }
+            };
+            return new Response(responseCode, responseData);
+        }
+    };
+    static TcpServer server = new TcpServer(protocol, SERVER_PORT);
+
     public static void main(String[] args) {
-        int port = 5000;
-
-        Protocol protocol = new ReverseLengthProtocol();
-        
-        TcpServer server = new TcpServer(protocol, port);
-        new Thread(server).start();
-
-        System.out.println("Reverse Length Server started on port " + port);
+        server.run();
     }
 }
