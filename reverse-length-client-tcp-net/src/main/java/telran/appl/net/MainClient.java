4eb861a34807416a1203567b430024f546b190a3
yanfi-sher <loss.soul20@gmail.com>
2024-11-07 14:26:02 +0200

    hw 38

--- reverse-length-client-tcp-net/src/main/java/telran/appl/net/MainClient.java
+++ reverse-length-client-tcp-net/src/main/java/telran/appl/net/MainClient.java
@@ -1,51 +1,55 @@
 package telran.appl.net;
-
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
 
 import telran.net.TcpClient;
 import telran.view.InputOutput;
 import telran.view.Item;
 import telran.view.Menu;
 import telran.view.StandardInputOutput;
-
 public class MainClient {
     static TcpClient client;
 
     public static void main(String[] args) {
-        Menu menu = new Menu("Network application",
-                Item.of("Start session", MainClient::startSession),
-                Item.of("Exit", MainClient::exit, true));
-        menu.perform(new StandardInputOutput());
+      Item[] items = {
+            Item.of("start session", MainClient::startSession),
+            Item.of("exit", MainClient::exit, true)
+      };
+      Menu menu = new Menu("Echo Application", items);
+      menu.perform(new StandardInputOutput());
     }
-
     static void startSession(InputOutput io) {
         String host = io.readString("Enter hostname");
-        int port = io.readNumberRange("Enter port (3000-50000)", "Wrong port", 3000, 50000).intValue();
-        tryingToClose();
-        client = new TcpClient(host, port);
-        Menu menu = new Menu("Run Session",
-                Item.of("Enter command and data", MainClient::stringProcessing),
-                Item.ofExit());
-        menu.perform(io);
-    }
-
-    static void stringProcessing(InputOutput io) {
-        String requestType = io.readString("Enter your command (reverse, lenght)");
-        String requestData = io.readString("Enter your data");
-        String response = client.sendAndReceive(requestType, requestData);
-        io.writeLine(response);
-    }
-
-    static void exit(InputOutput io) {
-        tryingToClose();
-    }
-
-    private static void tryingToClose() {
-        if (client != null) {
+        int port = io.readNumberRange("Enter port", "Wrong port", 3000, 50000).intValue();
+        if(client != null) {
             try {
                 client.close();
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
+            }
+        }
+        client = new TcpClient(host, port);
+        Menu menu = new Menu("Run Session",
+         Item.of("session", MainClient::sessionProcessing),
+          Item.ofExit());
+         menu.perform(io);
+
+    }
+    static void sessionProcessing(InputOutput io) {
+       HashSet<String> types = new HashSet<>(List.of("reverse", "length", "wrong_type_test"));
+        String type = io.readStringOptions("Enter operation type " + types, "Wrong type", types);
+        String string = io.readString("Enter any string");
+       
+        String response = client.sendAndReceive(type, string);
+        io.writeLine(response);
+    }
+    static void exit(InputOutput io) {
+        if(client != null) {
+            try {
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
--- reverse-length-server-tcp-net/src/main/java/telran/appl/net/MainServer.java
+++ reverse-length-server-tcp-net/src/main/java/telran/appl/net/MainServer.java
@@ -1,33 +1,12 @@
 package telran.appl.net;
 
-import telran.net.Protocol;
-import telran.net.Request;
-import telran.net.Response;
-import telran.net.ResponseCode;
 import telran.net.TcpServer;
 
 public class MainServer {
-    private static final int SERVER_PORT = 4000;
-
-    static Protocol protocol = new Protocol() {
-        @Override
-        public Response getResponse(Request request) {
-            String requestData = request.requestData();
-            ResponseCode responseCode = ResponseCode.OK;
-            String responseData = switch (request.requestType()) {
-                case "reverse" -> new StringBuilder(requestData).reverse().toString();
-                case "length" -> String.valueOf(requestData.length());
-                default -> {
-                    responseCode = ResponseCode.WRONG_TYPE;
-                    yield "Wrong command";
-                }
-            };
-            return new Response(responseCode, responseData);
-        }
-    };
-    static TcpServer server = new TcpServer(protocol, SERVER_PORT);
-
-    public static void main(String[] args) {
+     static final int PORT = 4000;
+    
+        public static void main(String[] args) {
+            TcpServer server = new TcpServer(new ReverseLengthProtocol(), PORT);
         server.run();
     }
 }
